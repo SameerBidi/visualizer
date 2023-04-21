b0c73fdfd2532e5625d98e7b3214424391440a82
SameerBidi <sambid1231998@gmail.com>
2023-04-22 01:17:31 +0530

    node linking half

--- Web/js/handler.js
+++ Web/js/handler.js
@@ -1,6 +1,8 @@
 const graph = [];
 const selectedNodes = [];
 
+let linkHolder, linkGroup, nodeUtilDiv, selNodeText, linkNodeDiv, nodeLinkCostInput, selInfoText, deleteNodeBtn;
+
 $(document).ready(function () {
 
 	let nodeHolder = $("div#node_holder");
@@ -10,6 +12,15 @@ $(document).ready(function () {
 	let addNodeNameInput = addModal.find("input#add_node_name_input");
 	let addHeuristicInput = addModal.find("input#add_heuristic_input");
 	let addNodeBtn = addModal.find("button#add_node_btn");
+
+	linkHolder = $("svg#link_holder");
+	linkGroup = $("g#link_group");
+	nodeUtilDiv = $("div#node_util_div");
+	selNodeText = nodeUtilDiv.find("p#sel_node_text");
+	linkNodeDiv = nodeUtilDiv.find("div#link_node_div");
+	nodeLinkCostInput = linkNodeDiv.find("input#node_link_cost");
+	selInfoText = nodeUtilDiv.find("p#sel_info_text");
+	deleteNodeBtn = nodeUtilDiv.find("button#delete_node_btn");
 
 	addModal.on("hide.bs.modal", function () {
 		addNodeBtn.off("click");
@@ -28,6 +39,8 @@ $(document).ready(function () {
 				alert("Please enter node name");
 				return;
 			}
+
+			heuristic && heuristic != "" ? heuristic = parseInt(heuristic) : heuristic = null;
 
 			let alreadyExists = false;
 			graph.forEach(function(node) {
@@ -51,11 +64,16 @@ $(document).ready(function () {
 			newNodeHeuristicText.html(heuristic);
 
 			newNode.on("click", function(e) {
-				if(selectedNodes.includes(newNodeId)) {
-					selectedNodes.splice(selectedNodes.indexOf(newNodeId), 1);
+				if(selectedNodes.map(x => x[1]).includes(newNodeId)) {
+					selectedNodes.splice(selectedNodes.findIndex(x => x[1] == newNodeId), 1);
 				}
 				else {
-					selectedNodes.push(newNodeId);
+					
+					for(let i = 0; i < selectedNodes.length - 1; i++) {
+						selectedNodes.splice(0, 1);
+					}
+
+					selectedNodes.push([nodeName, newNodeId]);
 				}
 
 				updateSelectedNodes();
@@ -71,7 +89,7 @@ $(document).ready(function () {
 
 			newNode.removeClass("d-none");
 
-			graph.push({ id: newNodeId, name: nodeName, heuristicValue: heuristic, neighbors: [] });
+			graph.push({ id: newNodeId, name: nodeName, heuristicValue: heuristic, neighbors: [], loc: [mousePos.x, mousePos.y] });
 
 			nodeHolder.append(newNode);
 
@@ -102,10 +120,41 @@ function updateSelectedNodes() {
 		nodeCircle.css("stroke", "white");
 	});
 
-	selectedNodes.forEach(function(nodeId) {
+	selectedNodes.forEach(function([nodeName, nodeId]) {
 		let node = $("#" + nodeId);
 		let nodeCircle = node.find("circle");
 
 		nodeCircle.css("stroke", "red");
 	});
+
+	if(selectedNodes.length > 0) {
+		nodeUtilDiv.removeClass("d-none");
+
+		selNodeText.html(selectedNodes.map(x => x[0]).join(", "));
+
+		selectedNodes.length == 1 ? deleteNodeBtn.removeClass("d-none") : deleteNodeBtn.addClass("d-none");
+
+		if(selectedNodes.length == 2) {
+			selInfoText.addClass("d-none");
+			linkNodeDiv.removeClass("d-none");
+		}
+		else {
+			linkNodeDiv.addClass("d-none");
+			selInfoText.removeClass("d-none");
+		}
+	}
+	else {
+		selNodeText.html("-");
+		nodeUtilDiv.addClass("d-none");
+	}
+}
+
+function linkNodes() {
+	if(selectedNodes.length != 2) {
+		alert("Please select exactly 2 nodes to link");
+		return;
+	}
+
+	let node1 = $("#" + selectedNodes[0][1]);
+	let node2 = $("#" + selectedNodes[1][1]);
 }
